a2a25becd6b942d7c7df1674dc7a1d8f2d1bc6e3
audi-gh <59245263+audi-gh@users.noreply.github.com>
2020-08-03 22:58:52 +0530

    Add files via upload

---
+++ PrimeFactorization.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 22:25:36 2020
+
+@author: Win10
+"""
+
+
+n=int(input("enter the number"))
+c=0
+l=[]
+if(n%2==0):
+    l.append(2)
+for i in range(3,n):
+    if(n%i==0 and i%2!=0):
+        l.append(i)
+for i in range(0,len(l)):
+    print(l[i])
+
+
+    
+   
+    
+    
+    
